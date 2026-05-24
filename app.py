a21e7ca46cff7c933e753693e8f8c5fb7cc2df76
Pedro Martins <pedromartins@MacBook.local>
2026-05-24 20:52:59 +0100

    Landing page Flask no Railway

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # Em desenvolvimento local
+    app.run(host="0.0.0.0", port=5000, debug=True)
